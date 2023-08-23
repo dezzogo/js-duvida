82fd060831765f10dbac1dffe10015650a96b6c8
dezzogo <hernandes_diogo@hotmail.com>
2023-08-23 19:41:26 -0300

    calculadora aprimorada!

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 const valor1 = document.querySelector('#num1');
 const operator = document.querySelector('#operator');
 const valor2 = document.querySelector('#num2');
-const botao=document.querySelector("#botao");
 let resultado = document.querySelector('#resultado');
 
 function calcular() {
-    if ( operator.value === "+") {
-        resultado.innerHTML = parseInt(valor1.value) + parseInt(valor2.value);
-    }
+    if (valor1.value!=='' && valor2.value!=='') {
+        if ( operator.value === "+") {
+            resultado.innerHTML = parseInt(valor1.value) + parseInt(valor2.value);
+        }
 
-    if ( operator.value === "-") {
-        resultado.innerHTML = parseInt(valor1.value) - parseInt(valor2.value);
-    }
+        if ( operator.value === "-") {
+            resultado.innerHTML = parseInt(valor1.value) - parseInt(valor2.value);
+        }
 
-    if ( operator.value === "*") {
-        resultado.innerHTML = parseInt(valor1.value) * parseInt(valor2.value);
-    }
+        if ( operator.value === "x") {
+            resultado.innerHTML = parseInt(valor1.value) * parseInt(valor2.value);
+        }
 
-    if ( operator.value === "/") {
-        resultado.innerHTML = parseInt(valor1.value) / parseInt(valor2.value);
+        if ( operator.value === "/") {
+            resultado.innerHTML = parseInt(valor1.value) / parseInt(valor2.value);
+        }
+    }  
+    else {
+        resultado.innerHTML='';
     }
 }
 
-botao.addEventListener("click",calcular);
+operator.addEventListener("change",calcular);
+valor1.addEventListener("keyup",calcular);
+valor2.addEventListener("keyup",calcular);
